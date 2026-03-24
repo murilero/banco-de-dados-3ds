c1b03375dbe39323eca5776185d2757b9198291b
murilero <murilo.dircksen.campos@escola.pr.gov.br>
2026-03-24 10:40:15 -0300

    Create sistema-livraria.sql

---
+++ sistema-livraria.sql
@@ -0,0 +1,14 @@
+-- Cria a tabela Clientes
+CREATE TABLE IF NOT EXISTS Clientes (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomeCliente VARCHAR(100) NOT NULL,
+    emailCliente VARCHAR(100) NOT NULL
+);
+
+-- Cria a tabela Compras
+CREATE TABLE IF NOT EXISTS Compras (
+    CompraID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ClienteID INTEGER NOT NULL,
+    NomeLivro VARCHAR(200) NOT NULL,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
+);
